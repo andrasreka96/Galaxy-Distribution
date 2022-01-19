1db5c902414c14469342ef5092fbdd0622460ad8
Reka Andras <andrasreka96@gmail.com>
2022-01-19 18:44:20 +0200

    mpi histograms

--- mpi/galaxy_mpi.c
+++ mpi/galaxy_mpi.c
@@ -9,61 +9,102 @@
 
 int main(int argc, char* argv[]) 
 {
- 
-  float *real_rasc, *real_decl, *rand_rasc, *rand_decl;
-  long int MemoryAllocatedCPU = 0L;
-  long int numberOfGalaxies = 100000;
-  int num_of_angle_intervals = 360;
 
-  float  pif = acosf(-1.0f);
-
-  /* this will store partial histogram calculations
-  by summing up the columns groupping the indixes in mod 3, we will get the final DR,DD and RR histograms */
-  long int ** histograms;
-
-  int np, me;
+  int np, myid;
   double starttime, endtime;
 
   MPI_Init(&argc, &argv);                /* Initialize MPI */
   MPI_Comm_size(MPI_COMM_WORLD, &np);    /* Get nr of processes */
-  MPI_Comm_rank(MPI_COMM_WORLD, &me);    /* Get own identifier */
-
-    
-  /* Check that we run on at least four processes */
-  if (np<4 ) {
-
-    if (me == 0) {
-      printf("You have to run the program on at least four processes\n");
-    }
-    MPI_Finalize();
-    exit(0);
-  }
+  MPI_Comm_rank(MPI_COMM_WORLD, &myid);    /* Get own identifier */
+ 
+  float *real_rasc, *real_decl, *rand_rasc, *rand_decl, *real_rasc_partial;
+  long int numberOfGalaxies = 100000;
+  int num_of_angle_intervals = 360;
+  float quantum_reciprocal = 4;
+  float radian_to_angle = 180 / acosf(-1.0f);
 
   // store right ascension and declination for real galaxies here
-  real_rasc        = (float *)calloc(numberOfGalaxies, sizeof(float));
-  real_decl        = (float *)calloc(numberOfGalaxies, sizeof(float));
+  real_rasc = (float *)calloc(numberOfGalaxies, sizeof(float));
+  real_decl = (float *)calloc(numberOfGalaxies, sizeof(float));
 
   // store right ascension and declination for synthetic random galaxies here
-  rand_rasc        = (float *)calloc(numberOfGalaxies, sizeof(float));
-  rand_decl        = (float *)calloc(numberOfGalaxies, sizeof(float));
-  
-  if (me == 0) {
+  rand_rasc = (float *)calloc(numberOfGalaxies, sizeof(float));
+  rand_decl = (float *)calloc(numberOfGalaxies, sizeof(float));
 
-      // read input data from files given on the command line
-      if ( parseargs_readinput(argc, argv, numberOfGalaxies, real_rasc, real_decl, rand_rasc, rand_decl) != 0 ) {printf("   Program stopped.\n");return(0);}
-      printf("   Input data read, now calculating histograms\n");
+  if ( parseargs_readinput(argc, argv, numberOfGalaxies, real_rasc, real_decl, rand_rasc, rand_decl) != 0 ) {printf("   Program stopped.\n");return(0);}
+    printf("   Input data read, now calculating histograms\n");
 
-      starttime = MPI_Wtime();
+  //init global histograms, these will store the result of reduce
+  long int histogram_DD_g[360] = {0L};
+  long int histogram_DR_g[360] = {0L};
+  long int histogram_RR_g[360] = {0L};
+ 
+  //init local histograms, these will store the partial calculations
+  long int histogram_DD_l[360] = {0L};
+  long int histogram_DR_l[360] = {0L};
+  long int histogram_RR_l[360] = {0L};
+
+
+  /* Start measuring time */
+  if (myid == 0) {
+    starttime = MPI_Wtime();
   }
-  
-  //broadcast to all other processes from root
-  MPI_Bcast(real_rasc, numberOfGalaxies, MPI_FLOAT, 0, MPI_COMM_WORLD);
-  MPI_Bcast(real_decl, numberOfGalaxies, MPI_FLOAT, 0, MPI_COMM_WORLD);
-  MPI_Bcast(rand_rasc, numberOfGalaxies, MPI_FLOAT, 0, MPI_COMM_WORLD);
-  MPI_Bcast(rand_decl, numberOfGalaxies, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
-  if (me != 0)
-    printf("   process %d running rand_decl %f\n", me, rand_decl[3]);
+  int p0_n =  np%3 == 0 ? np/3 : np/3 + np%3;
+  int p1_n = np/3;
+  int p2_n = p1_n;
 
-    return(0);
+  if(myid%3 == 0){
+    //calculates DR
+
+    int starti = (myid/3) * (numberOfGalaxies/p0_n);
+
+    for(int i=starti; i<starti+numberOfGalaxies/p0_n && i<numberOfGalaxies; ++i)
+        for(int j=0; j<numberOfGalaxies; ++j){
+          float angle = angle_between(real_rasc[i], real_decl[i], rand_rasc[j], rand_decl[j]) * radian_to_angle;
+          ++histogram_DR_l[ (int)(angle*quantum_reciprocal)];
+        }
+  }
+  else if (myid%3 == 1)
+  {
+    //calculates DD
+
+    int starti = (myid/3) * (numberOfGalaxies/p1_n);
+    for(int i=starti; i<starti+numberOfGalaxies/p1_n && i<numberOfGalaxies; ++i)
+         for(int j=i+1; j<numberOfGalaxies; ++j){
+            float angle = angle_between(real_rasc[i], real_decl[i], rand_rasc[j], rand_decl[j]) * radian_to_angle;
+            histogram_DD_l[ (int)(angle*quantum_reciprocal)] += 2;
+         }
+
+  }
+  else{
+      
+    //calculates RR
+
+    int starti = (myid/3) * (numberOfGalaxies/p2_n);
+    for(int i=starti; i<starti+numberOfGalaxies/p2_n && i<numberOfGalaxies; ++i)
+        for(int j=i+1; j<numberOfGalaxies; ++j){
+          float angle = angle_between(real_rasc[i], real_decl[i], rand_rasc[j], rand_decl[j]) * radian_to_angle;
+          histogram_RR_l[ (int)(angle*quantum_reciprocal)] += 2;
+        }
+
+
+  }
+
+  MPI_Reduce(histogram_DD_l, histogram_DD_g, numberOfGalaxies ,MPI_INT ,MPI_SUM ,0, MPI_COMM_WORLD);
+  MPI_Reduce(histogram_DR_l, histogram_DR_g, numberOfGalaxies ,MPI_INT ,MPI_SUM ,0, MPI_COMM_WORLD);
+  MPI_Reduce(histogram_RR_l, histogram_RR_g, numberOfGalaxies ,MPI_INT ,MPI_SUM ,0, MPI_COMM_WORLD);
+
+
+  if(myid==0)
+    {
+        printf("globalsum1 = %ld \n",histogram_DD_g[0]);
+        printf("globalsum2 = %ld \n",histogram_DD_g[1]);
+        printf("globalsum3 = %ld \n",histogram_DD_g[2]);
+
+    }
+
+    MPI_Finalize();
+
+  return (0);
 }
